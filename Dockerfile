b8d5e82afea25427575040ff07ddc19a2fbe48df
Ravindra Gollapelli <gravindra85@gmail.com>
2026-05-16 15:08:45 +0530

    jdk 11 changes

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,3 @@
-FROM java:8
+FROM eclipse-temurin:17-jre
 ADD ./target/gs-spring-boot-0.1.0.jar app.jar
 ENTRYPOINT ["java","-jar","app.jar"]
--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -10,11 +10,11 @@ import org.springframework.context.ApplicationContext;
 public class Application {
     
     public static void main(String[] args) {
-        ApplicationContext ctx = SpringApplication.run(Application.class, args);
+        var ctx = SpringApplication.run(Application.class, args);
         
         System.out.println("Let's inspect the beans provided by Spring Boot:");
         
-        String[] beanNames = ctx.getBeanDefinitionNames();
+        var beanNames = ctx.getBeanDefinitionNames();
         Arrays.sort(beanNames);
         for (String beanName : beanNames) {
             System.out.println(beanName);
